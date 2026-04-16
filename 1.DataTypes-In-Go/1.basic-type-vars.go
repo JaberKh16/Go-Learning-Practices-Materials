81c2083b06f452055b11373bf7ffec7d4e278a9f
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-16 19:54:41 +0600

    datatypes-in-go

---
+++ 1.DataTypes-In-Go/1.basic-type-vars.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Go Programming"
+	var age int = 10
+	var salary float64 = 50000.50
+	var isFun bool = true
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Salary:", salary)
+	fmt.Println("Is Go fun?", isFun)
+}
