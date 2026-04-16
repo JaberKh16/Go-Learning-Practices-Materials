d13175ee1933cddfe6b4b56be0c526961fad2a20
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-16 22:59:17 +0600

    datatypes-in-go

--- 1.DataTypes-In-Go/5.nil-type-var.go
+++ 1.DataTypes-In-Go/5.nil-type-var.go
@@ -1,3 +1,11 @@
+/*
+	Nil Type Var in Go
+	========================
+	In Go, the `nil` value represents the zero value for pointers, interfaces, maps, slices, channels, and function types. It indicates that a variable does not point to any valid data or object. When you declare a variable of one of these types without initializing it, it will have the `nil` value by default.
+
+	In the example below, we declare a variable named `nilValue` of type `interface{}` and assign it the `nil` value. This means that `nilValue` does not point to any valid data or object.
+*/
+
 package main
 
 import "fmt"
