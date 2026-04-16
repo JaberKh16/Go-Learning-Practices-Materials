b7e2b908c856edc44d12a1ad917e77b3aa9240f5
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-16 21:40:30 +0600

    datatypes-in-go

--- 1.DataTypes-In-Go/2.infered-type-var.go
+++ 1.DataTypes-In-Go/2.infered-type-var.go
@@ -1,9 +1,19 @@
+/*
+Infered Type Variables in Go
+============================
+In Go, you can declare variables without explicitly specifying their type. The Go compiler will infer
+the type based on the value assigned to the variable. This is known as "type inference".
+
+In the example below, we declare variables using the short variable declaration syntax (:=),
+which allows us to omit the type. The compiler will automatically determine the type of
+each variable based on the assigned value.
+*/
 package main
 
 import "fmt"
 
 func main() {
-	
+
 	name := "Go Programming"
 	age := 10
 	salary := 50000.50
