272fc391297dc55f5c3d53154058dafda6f091b0
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-17 05:21:10 +0600

    loop-in-go

---
+++ 3.Loops-In-Go/1.basic-for-loop.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Basic for loop
+	for idx := 0; idx < 5; idx++ {
+		fmt.Println("Iteration:", idx)
+	}
+
+}
---
+++ 3.Loops-In-Go/2.conditional-loop-basic.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	counter := 0
+
+	// conditional loop using for as a while loop => while loop use cases using for loop
+	for counter < 5 {
+		fmt.Println("Counter:", counter)
+		counter++
+	}
+}
+
+
