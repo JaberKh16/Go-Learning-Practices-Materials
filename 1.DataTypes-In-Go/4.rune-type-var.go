7f8e61e792c6abbdfd685ebdb18d76a61f7093cc
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-16 23:02:38 +0600

    datatypes-in-go

--- 1.DataTypes-In-Go/4.rune-type-var.go
+++ 1.DataTypes-In-Go/4.rune-type-var.go
@@ -1,3 +1,11 @@
+/*
+    Rune Type in Go
+	=========================
+	In Go, a `rune` is a data type that represents a Unicode code point. It is an alias for the `int32` type and can hold any valid Unicode character. Runes are typically used to represent characters in strings, and they can be represented using character literals (enclosed in single quotes).
+
+	In the example below, we declare a variable named `runeValue` of type `rune` and assign it the character 'A'. We then print both the character representation and the Unicode code point of the rune using formatted output.
+*/
+
 package main
 
 import "fmt"
---
+++ 1.DataTypes-In-Go/typecasting/1.typecasting-basic.go
@@ -0,0 +1,20 @@
+/*
+	Typecasting Variable In Go
+	===========================
+	In Go, you can perform typecasting to convert a variable from one data type to another. This is done using the syntax `Type(variable)`, where `Type` is the desired data type and `variable` is the value you want to convert.
+
+	In the example below, we declare a variable `age` of type `int` and assign it the value `10`. We then use typecasting to convert `age` to a `float64` and store it in a new variable called `ageFloat`. Finally, we print both the original integer value and the converted float value.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 10
+	var ageFloat float64 = float64(age) // Typecasting 'age' from int to float64
+
+	fmt.Println("Age (int):", age)
+	fmt.Println("Age (float64):", ageFloat)
+}
+
