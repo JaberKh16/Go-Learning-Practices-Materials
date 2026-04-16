3d7b55976b2987192427e3c289186511e5995902
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-16 21:27:41 +0600

    datatypes-in-go

---
+++ 1.DataTypes-In-Go/2.infered-type-var.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	name := "Go Programming"
+	age := 10
+	salary := 50000.50
+	isFun := true
+
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Salary:", salary)
+	fmt.Println("Is Go fun?", isFun)
+}
