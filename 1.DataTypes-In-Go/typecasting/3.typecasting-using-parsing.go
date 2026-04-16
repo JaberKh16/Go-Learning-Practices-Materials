5ebdbf47ce09eb1bfad7264b19bdbec8eeffc620
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-16 23:27:05 +0600

    datatypes-in-go

---
+++ 1.DataTypes-In-Go/typecasting/3.typecasting-using-parsing.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	age := 20
+
+	// typecasting using parsing
+	ageStr := fmt.Sprintf("%d", age) // Convert int to string using Sprintf
+
+	fmt.Println("Age (int):", age)
+	fmt.Println("Age (string):", ageStr)
+	fmt.Println("Type of age:", fmt.Sprintf("%T", age))
+	fmt.Println("Type of ageStr:", fmt.Sprintf("%T", ageStr))
+
+
+	// To convert a string back to an integer, you can use the strconv package
+	ageParsed, err := strconv.Atoi(ageStr) // Convert string back to int using Atoi
+	if err != nil {
+		fmt.Println("Error parsing age:", err)
+	} else {
+		fmt.Println("Parsed Age (int):", ageParsed)
+		fmt.Println("Type of ageParsed:", fmt.Sprintf("%T", ageParsed))
+	}
+}
