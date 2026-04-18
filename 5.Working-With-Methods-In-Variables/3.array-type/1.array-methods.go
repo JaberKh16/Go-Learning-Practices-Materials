ddf0d07fa3b49770a70d221dfd9328a27c886583
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-18 09:01:28 +0600

    methods-in-datatypes-in-go

---
+++ 5.Working-With-Methods-In-Variables/3.array-type/1.array-methods.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printArr(arr []int) {
+	// %v: prints the value in default format
+	fmt.Printf("%v\n", arr)
+	
+	// %+v: prints with field names for structs
+	// fmt.Printf("%+v\n", arr)
+	
+	// %#v: prints Go syntax representation
+	// fmt.Printf("%#v\n", arr)
+	
+	// %T: prints the type of the value
+	// fmt.Printf("%T\n", arr)
+}
+
+func performOperation(option string,  arr [] int ) [] {
+
+}
+
+func main(){
+	arr := [] int {1, 2, 10, 423, 565, 12}
+
+	// get the length
+	fmt.Println(len(arr))
+
+	// print the array
+	printArr(arr)
+
+
+	// append method
+	arr1 := apppend(55)
+	printArr(arr1)
+
+
+
+	// switch expression {
+	// case condition:
+		
+	// }
+	
+}
