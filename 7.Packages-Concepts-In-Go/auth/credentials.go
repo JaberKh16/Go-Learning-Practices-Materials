ca40c0b54c20817e9b960c27b473a8fbb09c1a18
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-21 04:23:24 +0600

    packages-concepts-in-go

--- 7.Packages-Concepts-In-Go/auth/credentials.go
+++ 7.Packages-Concepts-In-Go/auth/credentials.go
@@ -1,6 +1,25 @@
 package auth
 
+import "fmt"
+
 // convention to use Capital
-func LogginWithCredentials(username string, password string) (string, string) {
-	return username, password
+// func LogginWithCredentials(username string, password string) (string, string) {
+// 	return username, password
+// }
+
+
+type Credentials struct {
+	Username string
+	Password string
+}
+
+func LogginWithCredentials(username, password string) (Credentials, error) {
+	if username == "" || password == "" {
+		return Credentials{}, fmt.Errorf("invalid credentials")
+	}
+
+	return Credentials{
+		Username: username,
+		Password: password,
+	}, nil
 }
