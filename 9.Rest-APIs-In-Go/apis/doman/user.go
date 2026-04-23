82f51390411b2b05946c227dd2864df8f1ac781b
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-23 16:46:17 +0600

    rest-apis-concepts-in-go

--- 9.Rest-APIs-In-Go/apis/doman/user.go
+++ 9.Rest-APIs-In-Go/apis/doman/user.go
@@ -1,5 +1,16 @@
 package domain
 
+type Address struct {
+  addr string
+}
+
+type City struct {
+  name string
+  province string
+  zipcode string
+}
+
+
 type User struct {
 	ID        int    `json:"id"`
 	Name      string `json:"name"`
@@ -7,4 +18,6 @@ type User struct {
 	Gender    string `json:"gender"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
+	Address   Address
+	City      City
 }
