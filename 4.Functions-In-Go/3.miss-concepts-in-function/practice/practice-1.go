902c74a59f90bab60e6077dd125974c51285445b
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-23 12:49:28 +0600

    functions-in-go

---
+++ 4.Functions-In-Go/3.miss-concepts-in-function/practice/practice-1.go
@@ -0,0 +1,17 @@
+package main
+
+// fibonacci returns a function that returns fibonacci numbers
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func () int {
+		a, b = b, a + b
+		return a
+	}
+}
+
+func main(){
+	fib := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(fib())
+	}
+}
