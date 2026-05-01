5caf2ecc6d5d2a364d7203d3308aff4be56334fb
Jaber Khan <md.jaberkhan66@gmail.com>
2026-05-01 11:52:32 +0600

    practice-concepts-in-go

---
+++ 13.Practice-Concepts-In-Go/handlers/api-handler.go
@@ -0,0 +1,92 @@
+package handlers
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// root structure
+type SiteMapIndex struct{
+	XMLName xml.Name `xml:"sitemapindex"`
+	Sitemaps []Sitemap `xml:"sitemap"`
+}
+
+// each sitemap entry
+type Sitemap struct{
+	XMLName xml.Name `xml:"sitemap"`
+	Loc string `xml:"loc"`
+	LastMod string `xml:"lastmod"`
+}
+
+func WorkingWithXMLData(){
+	var data []byte
+	data := `<sitemapindex>
+	<sitemap>
+		<loc>https://...</loc>
+		<lastmod>2026-05-01</lastmod>
+	</sitemap>
+	</sitemapindex>`
+
+	// Parse the XML
+	var siteMapIndex SiteMapIndex
+	err := xml.Unmarshal(data, &siteMapIndex)
+	if err != nil {
+		panic(err)
+	}
+	
+	// Print the parsed data
+	println("Parsed sitemaps:")
+	for _, sitemap := range siteMapIndex.Sitemaps {
+		fmt.Println("URL: ", sitemap.Loc)
+		fmt.Println("Last Modified: ", sitemap.LastMod)
+		fmt.Println("-----------------------")
+	}
+}
+
+func PerformRequest() {
+	response, err := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	
+	// Read the response body
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	
+	// Parse the XML
+	var siteMapIndex SiteMapIndex
+	err = xml.Unmarshal(body, &siteMapIndex)
+	if err != nil {
+		panic(err)
+	}
+	
+	// Print the parsed data
+	println("Parsed sitemaps:")
+	for _, sitemap := range siteMapIndex.Sitemaps {
+		fmt.Println("URL: ", sitemap.Loc)
+		fmt.Println("Last Modified: ", sitemap.LastMod)
+		fmt.Println("-----------------------")
+	}
+}
+
+
+func FetchAllSitemaps(sitemaps []Sitemap) {
+	ch := make(chan string)
+
+	for _, s := range sitemaps {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			defer resp.Body.Close()
+			ch <- url
+		}(s.Loc)
+	}
+
+	for range sitemaps {
+		fmt.Println("Fetched:", <-ch)
+	}
+}
--- 13.Practice-Concepts-In-Go/main.go
+++ 13.Practice-Concepts-In-Go/main.go
@@ -7,43 +7,52 @@ import (
 	"time"
 	
 	"github.com/jaber/go-web/apis"
+	"github.com/jaber/go-web/handlers"
 	"github.com/jaber/go-web/middlewares"
 	"github.com/jaber/go-web/utitlities"
 )
 
 
-
-
-func main() {
-	http.HandleFunc("/", utitlities.HandleIndex)
-	http.HandleFunc("/file/serve", utitlities.HandleServeFile)
-
-
+func startTheHttpServer() error {
 	err := http.ListenAndServe(":8000", nil) // nil => DefaultServerMux
 	fmt.Println("Server started on http://localhost:8000")
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
+		return err
+	}
+	return nil
+}
+
+
+func main() {
+	// http.HandleFunc("/", utitlities.HandleIndex)
+	// http.HandleFunc("/file/serve", utitlities.HandleServeFile)
+
+
+	// Start server
+	if err := startTheHttpServer(); err != nil {
+		log.Fatal("ListenAndServe:", err)
 	}
 
 
 	time.Sleep(100 * time.Second)
 
 	// working with middleware
-	finalHandler := http.HandlerFunc(middlewares.Final)
-	http.Handle("/", middlewares.Middleware1(middlewares.Middleware2(finalHandler)))
+	// finalHandler := http.HandlerFunc(middlewares.Final)
+	// http.Handle("/", middlewares.Middleware1(middlewares.Middleware2(finalHandler)))
 
 
 	// call the instance
-	apis.InitializeWithStaticData()
+	// apis.InitializeWithStaticData()
 
 	// setup router
-	router := http.NewServeMux()
-	router.HandleFunc("/product/{id:[0-9]+}", utitlities.pageHandler)
+	// router := http.NewServeMux()
+	// router.HandleFunc("/product/{id:[0-9]+}", utitlities.pageHandler)
 
-	router.HandleFunc("/products", productHandler).
-Host("www.example.com").
-Methods("GET").
-Schemes("https")
+	// router.HandleFunc("/products", productHandler).Host("www.example.com").Methods("GET").Schemes("https")
+
+	// get the resposne
+	handlers.WorkingWithXMLData()
 }
 
 
